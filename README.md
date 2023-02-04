473c0f801ead815a88ce629895a61ca5f3f88e4c
DrAtomic <leo.ward.garcia@gmail.com>
2023-02-04 14:38:28 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Description
+
+wrappers for [highlight.el](https://www.emacswiki.org/emacs/highlight.el)
+
+# Installation
+
+put [highlight.el](https://www.emacswiki.org/emacs/highlight.el) in ~/.emacs.d/packages/hightlight/
+
+put [eyedropper.el](https://www.emacswiki.org/emacs/eyedropper.el) in ~/.emacs.d/packages/eyedropper/
+
+put [facemenu+.el](https://www.emacswiki.org/emacs/facemenu+.el) in ~/.emacs.d/packages/facemenu/
+
+put [hexrgb.el](https://www.emacswiki.org/emacs/hexrgb.el) in ~/.emacs.d/packages/hexrgb/
+
+then
+
+```bash
+git clone git@github.com:DrAtomic/hl-wrapper.git ~/.emacs.d/packages/
+
+```
+now add to your ~/.emacs.d/init.el
+
+```emacs-lisp
+(add-to-list 'load-path "~/.emacs.d/packages/highlight")
+(add-to-list 'load-path "~/.emacs.d/packages/facemenu")
+(add-to-list 'load-path "~/.emacs.d/packages/eyedropper")
+(add-to-list 'load-path "~/.emacs.d/packages/hexrgb")
+(add-to-list 'load-path "~/.emacs.d/packages/hl-wrapper")
+
+(require 'facemenu+)
+(require 'highlight)
+(require 'hl-wrapper)
+(global-set-key (kbd "C-c h b") 'hl-wrapper-highlight-region-blue)
+(global-set-key (kbd "C-c h r") 'hl-wrapper-highlight-region-red)
+(global-set-key (kbd "C-c h y") 'hl-wrapper-highlight-region-yellow)
+(global-set-key (kbd "C-c h g") 'hl-wrapper-highlight-region-green)
+```
---
+++ hl-wrapper.el
@@ -0,0 +1,103 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; simple wrappers around hlt-highlight
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'highlight)
+
+(defgroup hl-wrapper-faces nil
+  "Faces used by highlighing"
+  :group 'hl-wrapper
+  :group 'faces)
+
+;; from magit
+(defface hl-wrapper-red
+  `((((class color) (background light))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#eecccc"
+     :foreground "#aa2222")
+    (((class color) (background dark))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#663333"
+     :foreground "#eecccc"))
+  "Face for red"
+  :group 'hl-wrapper)
+
+;; from magit
+(defface hl-wrapper-green
+  `((((class color) (background light))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#cceecc"
+     :foreground "#22aa22")
+    (((class color) (background dark))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#336633"
+     :foreground "#cceecc"))
+  "Face for green"
+  :group 'hl-wrapper)
+
+(defface hl-wrapper-yellow
+  `((((class color) (background light))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#f7ffbc"
+     :foreground "#e5de00")
+    (((class color) (background dark))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#e6cc00"
+     :foreground "#f7ffbc"))
+  "Face for yellow"
+  :group 'hl-wrapper)
+
+(defface hl-wrapper-blue
+  `((((class color) (background light))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#d0efff"
+     :foreground "#187bcd")
+    (((class color) (background dark))
+     ,@(and (>= emacs-major-version 27) '(:extend t))
+     :background "#1167b1"
+     :foreground "#d0efff"))
+  "Face for-blue"
+  :group 'hl-wrapper)
+
+;;;###autoload
+(defun hl-wrapper-highlight-region-blue ()
+  (interactive)
+  (setq hlt-last-face 'hl-wrapper-blue)
+  (hlt-highlight-region))
+
+;;;###autoload
+(defun hl-wrapper-highlight-region-red ()
+  (interactive)
+  (setq hlt-last-face 'hl-wrapper-red)
+  (hlt-highlight-region))
+
+;;;###autoload
+(defun hl-wrapper-highlight-region-yellow ()
+  (interactive)
+  (setq hlt-last-face 'hl-wrapper-yellow)
+  (hlt-highlight-region))
+
+;;;###autoload
+(defun hl-wrapper-highlight-region-green ()
+  (interactive)
+  (setq hlt-last-face 'hl-wrapper-green)
+  (hlt-highlight-region))
+
+(provide 'hl-wrapper)
